67714e49e162784baf835ecb9203194e07657caa
Gabor Nagy <gaborka19890603@gmail.com>
2022-02-22 10:29:39 +0100

    Adjust FACTORY_ADDRESS for dev-trx

--- src/constants.ts
+++ src/constants.ts
@@ -20,13 +20,13 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0xed8185b97e71435f5c37381a8b4d050b5daee972'
+export const FACTORY_ADDRESS = '0x486be31557ee7d7b50113a512da8faa1a6c3a5ef'
 export const FACTORY_ADDRESSES: { [chainId: number]: string } = {
   [ChainId.MAINNET]: '0xC2301F81D7E05B70AC94207D4E5B7CB07C845568',
   [ChainId.NILE]: '0xed8185b97e71435f5c37381a8b4d050b5daee972',
-  [ChainId.SHASTA]: '0xed8185b97e71435f5c37381a8b4d050b5daee972'
+  [ChainId.SHASTA]: '0x486be31557ee7d7b50113a512da8faa1a6c3a5ef'
 }
-export const INIT_CODE_HASH = '0xecf3ad9b15e86f3ebbd9cb0ef01ca73f48a403eb2e294faa6057b1a364723bc5'
+export const INIT_CODE_HASH = '0x87abd74a1a26e0addc829240582ef115e4b42b6271fb9a3937363a3f0f756cce'
 
 export const MaxUint256 = JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
