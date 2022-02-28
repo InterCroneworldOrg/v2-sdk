8cf6c907afd5608118b6db99754c5f162b8401c0
Gabor Nagy <gaborka19890603@gmail.com>
2022-02-28 16:06:25 +0100

    Update to latest mainnet factory

--- src/constants.ts
+++ src/constants.ts
@@ -22,11 +22,11 @@ export enum Rounding {
 
 export const FACTORY_ADDRESS = '0x486be31557ee7d7b50113a512da8faa1a6c3a5ef'
 export const FACTORY_ADDRESSES: { [chainId: number]: string } = {
-  [ChainId.MAINNET]: '0xC2301F81D7E05B70AC94207D4E5B7CB07C845568',
+  [ChainId.MAINNET]: '0x6168bf722a3e7983e680f6bf3b2740cba7f92b7c',
   [ChainId.NILE]: '0xed8185b97e71435f5c37381a8b4d050b5daee972',
   [ChainId.SHASTA]: '0x486be31557ee7d7b50113a512da8faa1a6c3a5ef'
 }
-export const INIT_CODE_HASH = '0x87abd74a1a26e0addc829240582ef115e4b42b6271fb9a3937363a3f0f756cce'
+export const INIT_CODE_HASH = '0xaeab5e5d863dce4b9d24fa521d4e3b9915e19359bd891b4dc5ee022a2698541b'
 
 export const MaxUint256 = JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
